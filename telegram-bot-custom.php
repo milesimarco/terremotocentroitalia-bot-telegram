8bf52bed11a3a8bf12d152d0b87a682c8e774089
Marco Milesi <milesimarco@outlook.com>
2016-08-28 15:59:56 +0200

    added exif for files

--- telegram-bot-custom.php
+++ telegram-bot-custom.php
@@ -32,10 +32,48 @@ function telegramcustom_parse( $telegram_user_id, $text ) {
     return;
 }
 
-add_action('telegram_parse_photo','telegram_parse_photo_s', 10, 2);
+function telegram_parse_document_s ( $telegram_user_id, $document  ) {
+  $url = telegram_download_file( $telegram_user_id, $document['file_id'] );
+  if ( $url ) {
+
+    $plugin_post_id = telegram_getid( $telegram_user_id );
+    $local_url = ABSPATH.'wp-content/uploads/telegram-bot/'.$plugin_post_id.'/'.basename( $url );
+
+    if ( $geo = telegram_read_gps_location( $local_url ) ) {
+
+      $lat = $geo['lat'];
+      $lon = $geo['lon'];
+      $time = $geo['time'];
+
+      global $wpdb;
+
+      $wpdb->insert(
+          $wpdb->prefix . 'segnalazioni',
+          array(
+              'lat' => $lat,
+              'lon' => $lon,
+              'telegram_id' => $telegram_user_id,
+              'wordpress_id' => $plugin_post_id,
+              'description' => 'EXIF',
+              'url' => $url,
+              'time' => $time
+          ),
+          array( '%s', '%s', '%d', '%d', '%s' )
+      );
+      $int = telegram_location_haversine_distance ( 42.7, 13.24, $lat, $lon, $earthRadius = 6371);
+
+      telegram_sendmessage( $telegram_user_id, 'Foto registrata e geolocalizzata a '.$int.' km dall\'epicentro.  Grazie');
+      telegram_log('@@', '##', 'received file from '.$lat.' - '.$lon);
+    } else {
+      telegram_log('@@', '##', 'not compatible');
+    }
+  } else {
+    telegram_sendmessage( $telegram_user_id, 'Errore. Se il problema persiste contattaci');
+  }
+} add_action('telegram_parse_document','telegram_parse_document_s', 10, 2);
 
 function telegram_parse_photo_s ( $telegram_user_id, $photo  ) {
-  $url = telegram_download_photo( $telegram_user_id, $photo[2]['file_id'] );
+  $url = telegram_download_file( $telegram_user_id, $photo[2]['file_id'] );
   if ( $url ) {
     $plugin_post_id = telegram_getid( $telegram_user_id );
 
@@ -62,7 +100,7 @@ function telegram_parse_photo_s ( $telegram_user_id, $photo  ) {
     delete_post_meta( $plugin_post_id, 'telegram_custom_state' );
 
   }
-}
+} add_action('telegram_parse_photo','telegram_parse_photo_s', 10, 2);
 
 
 add_action('telegram_parse_location','telegramcustom_c_parse_location', 10, 3);
@@ -123,5 +161,47 @@ function telegram_c_plugin_parse_request() {
 	}
 }
 add_action('template_redirect', 'telegram_c_plugin_parse_request');
+
+function telegram_read_gps_location($file){
+    if (is_file($file)) {
+        $info = exif_read_data($file);
+        if (isset($info['GPSLatitude']) && isset($info['GPSLongitude']) &&
+            isset($info['GPSLatitudeRef']) && isset($info['GPSLongitudeRef']) &&
+            in_array($info['GPSLatitudeRef'], array('E','W','N','S')) && in_array($info['GPSLongitudeRef'], array('E','W','N','S'))) {
+
+            $GPSLatitudeRef  = strtolower(trim($info['GPSLatitudeRef']));
+            $GPSLongitudeRef = strtolower(trim($info['GPSLongitudeRef']));
+
+            $lat_degrees_a = explode('/',$info['GPSLatitude'][0]);
+            $lat_minutes_a = explode('/',$info['GPSLatitude'][1]);
+            $lat_seconds_a = explode('/',$info['GPSLatitude'][2]);
+            $lng_degrees_a = explode('/',$info['GPSLongitude'][0]);
+            $lng_minutes_a = explode('/',$info['GPSLongitude'][1]);
+            $lng_seconds_a = explode('/',$info['GPSLongitude'][2]);
+
+            $lat_degrees = $lat_degrees_a[0] / $lat_degrees_a[1];
+            $lat_minutes = $lat_minutes_a[0] / $lat_minutes_a[1];
+            $lat_seconds = $lat_seconds_a[0] / $lat_seconds_a[1];
+            $lng_degrees = $lng_degrees_a[0] / $lng_degrees_a[1];
+            $lng_minutes = $lng_minutes_a[0] / $lng_minutes_a[1];
+            $lng_seconds = $lng_seconds_a[0] / $lng_seconds_a[1];
+
+            $lat = (float) $lat_degrees+((($lat_minutes*60)+($lat_seconds))/3600);
+            $lng = (float) $lng_degrees+((($lng_minutes*60)+($lng_seconds))/3600);
+
+            //If the latitude is South, make it negative.
+            //If the longitude is west, make it negative
+            $GPSLatitudeRef  == 's' ? $lat *= -1 : '';
+            $GPSLongitudeRef == 'w' ? $lng *= -1 : '';
+
+            return array(
+                'lat' => $lat,
+                'lon' => $lng,
+                'time' => $info['DateTimeOriginal']
+            );
+        }
+    }
+    return false;
+}
 
 ?>
